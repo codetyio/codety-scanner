b815fcd1b76728cd4510f909bfc9516840357e5d
Tony <3987237+random1223@users.noreply.github.com>
2024-08-28 22:43:15 -0700

    fix_golangcilint_empty_result_error (#6)

---
+++ analyzer-scalastyle/doc/doc.md
@@ -0,0 +1,3 @@
+
+Doc:
+https://www.scalastyle.org/
---
+++ code-issue-examples/scala/test.scala
@@ -0,0 +1,8 @@
+object OrderService {
+  def make (
+             userService: UserService,
+             discountForProuduct: DiscountService,
+             bonusForPurchase: BonusesService,
+             productIdIsValid: ProductService
+           ): OrderService
+}
