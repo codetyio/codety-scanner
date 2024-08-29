16025bdc09df0285e114cb332567a333f788b7ce
Tony <3987237+random1223@users.noreply.github.com>
2024-08-29 16:12:53 -0700

    Add scalastyle dtos (#9)

---
+++ scanner-common/src/main/java/io/codety/scanner/util/XmlFactoryUtil.java
@@ -0,0 +1,9 @@
+package io.codety.scanner.util;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class XmlFactoryUtil {
+    public static final XmlMapper xmlMapper = new XmlMapper();
+
+            ;
+}
