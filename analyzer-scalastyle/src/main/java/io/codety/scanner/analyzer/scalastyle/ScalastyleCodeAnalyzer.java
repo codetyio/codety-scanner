88dd9183e22a0883709ab0cf985f220e3c1d0784
tsun@codety.io <tsun@codety.io>
2024-08-29 23:44:05 -0700

    Fix new line in comments.

--- analyzer-scalastyle/src/main/java/io/codety/scanner/analyzer/scalastyle/ScalastyleCodeAnalyzer.java
+++ analyzer-scalastyle/src/main/java/io/codety/scanner/analyzer/scalastyle/ScalastyleCodeAnalyzer.java
@@ -17,7 +17,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class ScalastyleCodeAnalyzer implements CodeAnalyzerInterface {
