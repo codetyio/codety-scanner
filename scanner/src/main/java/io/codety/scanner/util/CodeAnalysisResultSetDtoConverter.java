7afc5b918052def66246a5b618a0c5b5d3d8e951
catchcatchus <156882337+catchcatchus@users.noreply.github.com>
2024-09-05 11:25:11 -0700

    Changes. (#41)

--- scanner/src/main/java/io/codety/scanner/util/CodeAnalysisResultSetDtoConverter.java
+++ scanner/src/main/java/io/codety/scanner/util/CodeAnalysisResultSetDtoConverter.java
@@ -1,21 +1,34 @@
 package io.codety.scanner.util;
 
+import io.codety.scanner.reporter.dto.CodeAnalysisIssueDto;
 import io.codety.scanner.reporter.dto.CodeAnalysisResultDto;
 import io.codety.scanner.reporter.dto.CodeAnalysisResultSetDto;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CodeAnalysisResultSetDtoConverter {
-    public static void mergeByLanguage(CodeAnalysisResultSetDto codeAnalysisResultSetDto) {
+    public static List<CodeAnalysisResultDto> mergeByLanguage(CodeAnalysisResultSetDto codeAnalysisResultSetDto) {
 
         List<CodeAnalysisResultDto> codeAnalysisResultDtoList = codeAnalysisResultSetDto.getCodeAnalysisResultDtoList();
+        List<CodeAnalysisResultDto> mergedResultList = new ArrayList<>();
+        Map<String, CodeAnalysisResultDto> map = new HashMap<>();
+        for(CodeAnalysisResultDto resultDto : codeAnalysisResultDtoList){
+            String language = resultDto.getLanguage();
+            if(map.containsKey(language)) {
+                Map<String, List<CodeAnalysisIssueDto>> issuesByFile = resultDto.getIssuesByFile();
+                for(String file : issuesByFile.keySet()){
+                    map.get(language).addIssues(issuesByFile.get(file));
+                }
 
-        Map<String, List<CodeAnalysisResultDto>> map = new HashMap<>();
-        
-
-
+            }else{
+                map.put(language, resultDto);
+                mergedResultList.add(resultDto);
+            }
+        }
+        return mergedResultList;
 
     }
 }
