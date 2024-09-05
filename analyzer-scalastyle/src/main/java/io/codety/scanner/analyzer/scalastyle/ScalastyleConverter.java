21ded54a548b336cfa1e4edd5b30f05c68667d34
catchcatchus <eye@codety.io>
2024-09-05 11:31:16 -0700

    Changes.

--- analyzer-scalastyle/src/main/java/io/codety/scanner/analyzer/scalastyle/ScalastyleConverter.java
+++ analyzer-scalastyle/src/main/java/io/codety/scanner/analyzer/scalastyle/ScalastyleConverter.java
@@ -5,6 +5,7 @@ import io.codety.scanner.analyzer.scalastyle.dto.ScalastyleCheckstyle;
 import io.codety.scanner.analyzer.scalastyle.dto.ScalastyleError;
 import io.codety.scanner.analyzer.scalastyle.dto.ScalastyleFile;
 import io.codety.scanner.reporter.dto.CodeAnalysisIssueDto;
+import io.codety.scanner.util.CodetyConsoleLogger;
 import io.codety.scanner.util.XmlFactoryUtil;
 
 import java.util.ArrayList;
@@ -18,34 +19,36 @@ public class ScalastyleConverter {
         try {
             ScalastyleCheckstyle scalastyleCheckstyle = XmlFactoryUtil.xmlMapper.readValue(payload, ScalastyleCheckstyle.class);
             List<ScalastyleFile> files = scalastyleCheckstyle.getFile();
-            for(ScalastyleFile scalastyleFile : files){
+            if(files!=null) {
+                for (ScalastyleFile scalastyleFile : files) {
 
-                String fileName = scalastyleFile.getName();
-                if(fileName.startsWith("/")){
-                    fileName = fileName.substring(1);
-                }
-                List<ScalastyleError> errors = scalastyleFile.getError();
-                for(ScalastyleError error : errors){
-                    CodeAnalysisIssueDto issueDto = new CodeAnalysisIssueDto();
-                    issueDto.setFilePath(fileName);
-                    String severity = error.getSeverity();
-                    issueDto.setPriority(3);
-                    issueDto.setIssueCategory("generic");
-                    String source = error.getSource();
-                    if(source != null){
-                        String[] split = source.split("\\.");
-                        source = split[split.length-1];
+                    String fileName = scalastyleFile.getName();
+                    if (fileName.startsWith("/")) {
+                        fileName = fileName.substring(1);
                     }
-                    issueDto.setIssueCode(source);
-                    issueDto.setStartLineNumber(error.getLine() == null ? 1 : error.getLine());
-                    issueDto.setDescription(error.getMessage());
-                    codeAnalysisIssueDtos.add(issueDto);
+                    List<ScalastyleError> errors = scalastyleFile.getError();
+                    for (ScalastyleError error : errors) {
+                        CodeAnalysisIssueDto issueDto = new CodeAnalysisIssueDto();
+                        issueDto.setFilePath(fileName);
+                        String severity = error.getSeverity();
+                        issueDto.setPriority(3);
+                        issueDto.setIssueCategory("generic");
+                        String source = error.getSource();
+                        if (source != null) {
+                            String[] split = source.split("\\.");
+                            source = split[split.length - 1];
+                        }
+                        issueDto.setIssueCode(source);
+                        issueDto.setStartLineNumber(error.getLine() == null ? 1 : error.getLine());
+                        issueDto.setDescription(error.getMessage());
+                        codeAnalysisIssueDtos.add(issueDto);
+                    }
                 }
             }
 
 
         } catch (JsonProcessingException e) {
-
+            CodetyConsoleLogger.debug("Failed to parse scalatyle result", e);
         }
 
 
