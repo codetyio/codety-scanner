0536ab9013d1ce75a677ec76bbce0b957665021f
catchcatchus <eye@codety.io>
2024-09-05 14:24:53 -0700

    update ruby analyzer.

--- scanner-common/src/main/java/io/codety/scanner/util/StringUtil.java
+++ scanner-common/src/main/java/io/codety/scanner/util/StringUtil.java
@@ -19,4 +19,24 @@ public class StringUtil {
 
     }
 
+    public static String camelCaseSentencesToDashConnectedSentences(String str){
+        if(str == null){
+            return str;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(i == 0) {
+                builder.append(Character.toLowerCase(c));
+            }else if(Character.isUpperCase(c)){
+                builder.append("-").append(Character.toLowerCase(c));
+            }else{
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+
+    }
+
 }
