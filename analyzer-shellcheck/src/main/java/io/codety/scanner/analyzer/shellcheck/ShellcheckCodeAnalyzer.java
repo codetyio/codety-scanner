b0dc6aee59c8a97badf9a520b3a5e685278a1c60
limpidsa <tony.sun.dev@gmail.com>
2024-09-08 15:33:47 -0700

    Update shellcheck classes.

--- analyzer-shellcheck/src/main/java/io/codety/scanner/analyzer/shellcheck/ShellcheckCodeAnalyzer.java
+++ analyzer-shellcheck/src/main/java/io/codety/scanner/analyzer/shellcheck/ShellcheckCodeAnalyzer.java
@@ -4,10 +4,14 @@ import io.codety.common.dto.CodeAnalyzerType;
 import io.codety.common.dto.LanguageType;
 import io.codety.scanner.analyzer.CodeAnalyzerInterface;
 import io.codety.scanner.analyzer.dto.AnalyzerConfigurationDetailDto;
+import io.codety.scanner.reporter.dto.CodeAnalysisIssueDto;
 import io.codety.scanner.reporter.dto.CodeAnalysisResultDto;
 import io.codety.scanner.service.dto.AnalyzerRequest;
+import io.codety.scanner.util.CodetyConsoleLogger;
+import io.codety.scanner.util.RuntimeExecUtil;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +19,42 @@ import java.util.List;
 public class ShellcheckCodeAnalyzer implements CodeAnalyzerInterface {
     @Override
     public List<CodeAnalysisResultDto> analyzeCode(AnalyzerConfigurationDetailDto runnerConfiguration, AnalyzerRequest request) {
-        return new ArrayList<>();
+
+        ArrayList<CodeAnalysisResultDto> codeAnalysisResultDtos = new ArrayList<>();
+
+//        File file = runnerConfiguration.getFile();
+//cppcheck  . --suppressions-list=suppression.txt  --xml 2>error1.txt
+
+        String[] command;
+
+        String localGitRepoPath = request.getLocalGitRepoPath();
+        if(runnerConfiguration.getPayload() == null || runnerConfiguration.getPayload().isEmpty()){
+            command = new String[]{"shellcheck", "--format=json", localGitRepoPath + "/*"};
+        }else{
+            command = new String[]{"shellcheck", "--format=json", localGitRepoPath + "/*"};
+        }
+        try {
+
+            RuntimeExecUtil.RuntimeExecResult runtimeExecResult = RuntimeExecUtil.exec(command, null, 60, false, null);
+
+            CodeAnalysisResultDto resultDto = new CodeAnalysisResultDto(runnerConfiguration.getLanguage(), runnerConfiguration.getCodeAnalyzerType());
+            codeAnalysisResultDtos.add(resultDto);
+
+            String errorOutput = runtimeExecResult.getErrorOutput();
+            String successOutput = runtimeExecResult.getSuccessOutput();
+            if(errorOutput!=null && errorOutput.length() > 0){
+                CodetyConsoleLogger.debug("Warnings from rubucop: " + errorOutput);
+            }
+            if(successOutput!=null && successOutput.length() > 0) {
+                List<CodeAnalysisIssueDto> codeAnalysisIssueDtoList = ShellcheckConverter.convertResult(successOutput, localGitRepoPath);
+                resultDto.addIssues(codeAnalysisIssueDtoList);
+            }
+        } catch (Exception e) {
+            CodetyConsoleLogger.debug("Failed to run Rubocop analyzer ", e);
+            CodetyConsoleLogger.info("Failed to run Rubocop analyzer " + e.getMessage());
+        }
+
+        return codeAnalysisResultDtos;
     }
 
     @Override
