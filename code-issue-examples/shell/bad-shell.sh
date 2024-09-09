0942f078ef23deea33bf31276b864c5cb485f6d5
limpidsa <158790119+limpidsa@users.noreply.github.com>
2024-09-08 21:48:46 -0700

    Update shellcheck classes.

--- code-issue-examples/shell/bad-shell.sh
+++ code-issue-examples/shell/bad-shell.sh
@@ -1,6 +1,6 @@
- echo $1                           # Unquoted variables
- find . -name *.ogg                # Unquoted find/grep patterns
- rm "~/my file.txt"                # Quoted tilde expansion
- v='--verbose="true"'; cmd $v      # Literal quotes in variables
+echo $1                           # Unquoted variables
+find . -name *.ogg                # Unquoted find/grep patterns
+rm "~/my file.txt"                # Quoted tilde expansion
+v='--verbose="true"'; cmd $v      # Literal quotes in variables
 
- find /src -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) | xargs shellcheck
+find /src -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) | xargs shellcheck
