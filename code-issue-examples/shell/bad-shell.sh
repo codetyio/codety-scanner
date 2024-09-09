cd814b2679faa5e4adc11315c14972c15ad12370
limpidsa <158790119+limpidsa@users.noreply.github.com>
2024-09-08 21:46:15 -0700

    Update shellcheck classes. (#60)

--- code-issue-examples/shell/bad-shell.sh
+++ code-issue-examples/shell/bad-shell.sh
@@ -2,13 +2,5 @@
  find . -name *.ogg                # Unquoted find/grep patterns
  rm "~/my file.txt"                # Quoted tilde expansion
  v='--verbose="true"'; cmd $v      # Literal quotes in variables
- for f in "*.ogg"                  # Incorrectly quoted 'for' loops
- touch $@                          # Unquoted $@
- echo 'Don't forget to restart!'   # Singlequote closed by apostrophe
- echo 'Don\'t try this at home'    # Attempting to escape ' in ''
- echo 'Path is $PATH'              # Variables in single quotes
- trap "echo Took ${SECONDS}s" 0    # Prematurely expanded trap
- unset var[i]                      # Array index treated as glob
-
 
  find /src -type f \( -name '*.sh' -o -name '*.bash' -o -name '*.ksh' -o -name '*.bashrc' -o -name '*.bash_profile' -o -name '*.bash_login' -o -name '*.bash_logout' \) | xargs shellcheck
