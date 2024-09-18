a87e6ed3465a092024dbc3fe6a9cf933a5f5067f
limpidsa <158790119+limpidsa@users.noreply.github.com>
2024-09-18 08:19:17 -0700

    Add phpstan analyzer.

---
+++ analyzer-phpstan/src/main/java/io/codety/scanner/analyzer/phpstan/PhpstanCodeAnalyzer.java
@@ -0,0 +1,61 @@
+package io.codety.scanner.analyzer.phpstan;
+
+import io.codety.common.dto.CodeAnalyzerType;
+import io.codety.common.dto.LanguageType;
+import io.codety.scanner.analyzer.CodeAnalyzerInterface;
+import io.codety.scanner.analyzer.dto.AnalyzerConfigurationDetailDto;
+import io.codety.scanner.reporter.dto.CodeAnalysisIssueDto;
+import io.codety.scanner.reporter.dto.CodeAnalysisResultDto;
+import io.codety.scanner.service.dto.AnalyzerRequest;
+import io.codety.scanner.util.CodetyConsoleLogger;
+import io.codety.scanner.util.RuntimeExecUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PhpstanCodeAnalyzer implements CodeAnalyzerInterface {
+    @Override
+    public List<CodeAnalysisResultDto> analyzeCode(AnalyzerConfigurationDetailDto runnerConfiguration, AnalyzerRequest request) {
+
+        CodetyConsoleLogger.info("Scanning "+runnerConfiguration.getLanguage()+" code via "+runnerConfiguration.getCodeAnalyzerType().name()+"...");
+        ArrayList<CodeAnalysisResultDto> list = new ArrayList();
+
+        String[] command;
+        String localGitRepoPath = request.getLocalGitRepoPath();
+        //./vendor/bin/phpstan --memory-limit=1024000000  --no-interaction --no-progress  --error-format=json  analyse
+        if(runnerConfiguration.getPayload() == null || runnerConfiguration.getPayload().isEmpty()){
+            command = new String[]{"./vendor/bin/phpsta", "--memory-limit=1024000000", "--no-interaction", "--no-progress", "--error-format=json", "analyse", localGitRepoPath};
+        }else{
+            //Use multiple rules: --check CKV_GCP_33,CKV_GCP_34,CKV_GCP_35 ...
+            command = new String[]{"./vendor/bin/phpsta", "--memory-limit=1024000000", "--no-interaction", "--no-progress", "--error-format=json", "analyse", localGitRepoPath};
+        }
+        try {
+            RuntimeExecUtil.RuntimeExecResult runtimeExecResult = RuntimeExecUtil.exec(command, "/", 60, false, null);
+
+            String errorOutput = runtimeExecResult.getErrorOutput();
+            String successOutput = runtimeExecResult.getSuccessOutput();
+
+            List<CodeAnalysisIssueDto> codeAnalysisIssueDtoList = PhpstanConverter.convertResult(successOutput, localGitRepoPath);
+
+            CodeAnalysisResultDto resultDto = new CodeAnalysisResultDto(runnerConfiguration.getLanguage(), runnerConfiguration.getCodeAnalyzerType());
+            resultDto.setDisplayTitle("IaC");
+            resultDto.addIssues(codeAnalysisIssueDtoList);
+            list.add(resultDto);
+
+        } catch (Exception e) {
+            CodetyConsoleLogger.info("Skip checkov analyzer due to exceptions");
+            CodetyConsoleLogger.debug("Skip checkov analyzer due to exceptions " + e.getMessage(), e);
+        }
+
+        return list;
+
+    }
+
+    @Override
+    public List<CodeAnalysisResultDto> analyzeCode(AnalyzerRequest request) {
+        return analyzeCode(new AnalyzerConfigurationDetailDto(LanguageType.php, CodeAnalyzerType.phpstan), request);
+    }
+}
