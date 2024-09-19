92f60b59bb7b77457608daf770a8a508d588ff68
limpidsa <158790119+limpidsa@users.noreply.github.com>
2024-09-18 22:11:09 -0700

    Add phpstan analyzer.

--- analyzer-phpstan/src/main/java/io/codety/scanner/analyzer/phpstan/PhpstanConverter.java
+++ analyzer-phpstan/src/main/java/io/codety/scanner/analyzer/phpstan/PhpstanConverter.java
@@ -2,6 +2,7 @@ package io.codety.scanner.analyzer.phpstan;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.codety.scanner.analyzer.phpstan.dto.PhpstanIssueDto;
+import io.codety.scanner.analyzer.phpstan.dto.PhpstanMessage;
 import io.codety.scanner.analyzer.phpstan.dto.PhpstanRoot;
 import io.codety.scanner.reporter.dto.CodeAnalysisIssueDto;
 import io.codety.scanner.util.JsonFactoryUtil;
@@ -11,18 +12,37 @@ import java.util.List;
 import java.util.Map;
 
 public class PhpstanConverter {
+
+    private static final int defaultPriority = 3;
+
     public static List<CodeAnalysisIssueDto> convertResult(String successOutput, String localGitRepoPath) throws JsonProcessingException {
         List<CodeAnalysisIssueDto> result = new ArrayList<>();
         PhpstanRoot phpstanRoot = JsonFactoryUtil.objectMapper.readValue(successOutput, PhpstanRoot.class);
 
         Map<String, PhpstanIssueDto> files = phpstanRoot.getFiles();
         for(String file : files.keySet()){
-            if(file.startsWith(localGitRepoPath)){
-
+            String filePath = file;
+            if(filePath.startsWith(localGitRepoPath)){
+                filePath = filePath.substring(localGitRepoPath.length()+1);
             }
-
+            PhpstanIssueDto phpstanIssueDto = files.get(file);
+            ArrayList<PhpstanMessage> messages = phpstanIssueDto.getMessages();
+            if(messages== null){
+                continue;
+            }
+            for(PhpstanMessage phpstanMessage : messages){
+                String message = phpstanMessage.getTip();
+                CodeAnalysisIssueDto issueDto = new CodeAnalysisIssueDto();
+                issueDto.setFilePath(filePath);
+                issueDto.setStartLineNumber(phpstanMessage.getLine());
+                String identifier = phpstanMessage.getIdentifier();
+                issueDto.setIssueCategory(identifier);
+                issueDto.setDescription(message);
+                issueDto.setIssueCode(identifier);
+                issueDto.setPriority(defaultPriority);
+                result.add(issueDto);
+            }
         }
-
 
         return result;
     }
